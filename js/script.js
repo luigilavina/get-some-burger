41908f99538640ef1c629ddf3b0907527c76f143
Luigi Lavina <lavinaluigi@gmail.com>
2026-05-04 10:24:47 -0300

    implementar menu interativo via JS

--- js/script.js
+++ js/script.js
@@ -25,3 +25,49 @@ btnVolta.addEventListener('click', () => {
   indice = (indice - 1 + imagens.length) % imagens.length;
   atualizarImagem();
 });
+
+
+// Menu Fixo
+
+const dados = {
+  hamburgers: [
+    { nome: "Get Some Classic", preco: 20 },
+    { nome: "Get Some Double Meat", preco: 25 }
+  ],
+  acompanhamentos: [
+    { nome: "Batata frita", preco: 15 }
+  ],
+  sobremesas: [
+    { nome: "Milkshake", preco: 18 }
+  ],
+  bebidas: [
+    { nome: "Coca-Cola", preco: 8 }
+  ],
+  drinks: [
+    { nome: "Caipirinha", preco: 22 }
+  ]
+};
+
+const categoria = document.querySelectorAll('.categoria')
+const areaItens = document.getElementById('area-itens')
+
+categoria.forEach(cat =>{
+  cat.addEventListener('click', () =>{
+    const tipo = cat.dataset.categoria
+
+    mostrarItens(tipo)
+  })
+}) 
+
+function mostrarItens(tipo){
+  areaItens.innerHTML = ''
+
+  dados[tipo].forEach(item =>{
+    const div = document.createElement('div')
+
+    div.classList.add('item-card')
+    div.textContent = `${item.nome} - R$ ${item.preco}`;
+
+    areaItens.appendChild(div)
+  })
+}
